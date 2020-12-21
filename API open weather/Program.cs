c55ce07e6fe67c407d06a21ae1d85e211cda84d2
Labenas <66065598+Labenas@users.noreply.github.com>
2020-12-21 11:16:47 +0200

    Add project files.

---
+++ API open weather/Program.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace API_open_weather
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+            Console.WriteLine("Enter city you would like weather forecast to receive");
+            var cityName = Console.ReadLine().ToLower();
+
+            var apiKey = "ade19c9281423758e4494e4bb8119c22";
+
+
+
+            var responseWeather = await client.GetAsync($"http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=metric&lang=lt&appid=" + apiKey);
+
+
+            var responseWeatherBody = await responseWeather.Content.ReadAsStringAsync();
+
+
+            var myDeserializedClass = JsonConvert.DeserializeObject<Root>(responseWeatherBody);
+
+            
+           
+            Console.WriteLine($"Current temperature in {cityName} is {myDeserializedClass.main.temp}C and it feels like {myDeserializedClass.main.feels_like}C  Today's day temperature from {myDeserializedClass.main.temp_min}C to {myDeserializedClass.main.temp_max}C");
+           
+        }
+            
+    }
+}
